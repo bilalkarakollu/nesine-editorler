ddc9443db16e778d419d93f503d60c926282bed3
Bilal Karakollu <karakollubilal@gmail.com>
2024-05-02 08:09:10 +0300

    Add Bootstrap dependency and import Bootstrap CSS

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,9 +14,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-          rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-          crossOrigin="anonymous"></link>
         <link
           rel="stylesheet"
           type="text/css"
